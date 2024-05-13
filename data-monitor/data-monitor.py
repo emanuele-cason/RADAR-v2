f0e7b10c05aa7b63a8478b3f6a0034d87c385a28
Emanuele Cason <emanuele.cason@gmail.com>
2024-05-13 11:39:17 +0200

    Aggiunto radiobutton tracking, selezione buffer, markers

--- data-monitor/data-monitor.py
+++ data-monitor/data-monitor.py
@@ -2,11 +2,12 @@ import serial
 import struct
 
 import threading
+import time
+import random
 
 import dearpygui.dearpygui as dpg
-import time
-
 import pyautogui
+
 
 screen_width, screen_height = pyautogui.size()
 screen_height *= 0.9
@@ -42,7 +43,14 @@ data_label[voltage] = "Tensione [V]"
 data_label[current] = "Corrente [A]"
 
 # Dati dei plot
-plot1_buffer_size = 1000
+marker_colors = [
+    [102,187,106, 255],    # Verde
+    [66,165,245, 255],    # Blu
+    [239,83,80, 255],    # Rosso
+    [255,202,40, 255],  # Giallo
+    [171,71,188, 255],  # Viola
+]
+
 plot1_data_y = []
 plot1_data_x = []
 plot1_data_ID = 11
@@ -112,6 +120,14 @@ def plot_data_selected(sender, app_data, user_data):
         dpg.add_line_series(plot1_data_x, plot1_data_y, label=data_label[plot1_data_ID], parent="PL1-YA", tag="PL1-YA-serie")
         dpg.configure_item("PL1-YA", label=data_label[plot1_data_ID])
         dpg.bind_item_theme("PL1-YA-serie", "PL-T")
+
+def plot_add_marker(sender, app_data, user_data):
+    global marker_i
+    marker_i +=1
+    
+    if sender == "PL1-M":
+        dpg.add_plot_annotation(parent="PL1", label=f"M{marker_i}", default_value=(plot1_data_x[-1], plot1_data_y[-1]), color=marker_colors[random.randint(0, len(marker_colors)-1)])
+    
     
 with dpg.window(label="Real time plot #1", pos=[screen_width/4, 0], height=screen_height/2, width=screen_width*0.75):
 
@@ -120,11 +136,13 @@ with dpg.window(label="Real time plot #1", pos=[screen_width/4, 0], height=scree
             dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 3, category=dpg.mvThemeCat_Plots)
 
     with dpg.group(horizontal=True):
-        dpg.add_combo(data_label, height_mode=dpg.mvComboHeight_Small, tag="PL1-C", default_value=data_label[plot1_data_ID], callback=plot_data_selected)
-        dpg.add_checkbox(label="Auto-track", tag="PL1-AT", default_value=True)
-        dpg.add_checkbox(label="Follow-last", tag="PL1-FL", default_value=False)
-
-    with dpg.plot(label="PL1", height=screen_height/2.5, width=screen_width*0.74):
+        dpg.add_combo(data_label, width=200, height_mode=dpg.mvComboHeight_Small, tag="PL1-C", default_value=data_label[plot1_data_ID], callback=plot_data_selected)
+        dpg.add_radio_button(items=("Manuale","Completo", "Insegui"), horizontal=True, default_value="Completo", tag="PL1-RB")
+        dpg.add_slider_int(label="Buffer", tag="PL1-B", default_value=1000, min_value=100, max_value=5000, width=200)
+        dpg.add_button(tag="PL1-M", label="Marker", callback=plot_add_marker)
+        marker_i = 0
+        
+    with dpg.plot(tag="PL1", height=screen_height/2.5, width=screen_width*0.74):
 
         dpg.add_plot_axis(dpg.mvXAxis, label=" ", tag="PL1-XA", time=True)
         dpg.add_plot_axis(dpg.mvYAxis, label=data_label[plot1_data_ID], tag="PL1-YA")
@@ -143,18 +161,21 @@ while dpg.is_dearpygui_running():
     plot1_data_y.append(data[plot1_data_ID])
     plot1_data_x.append(time.time())
 
-    if dpg.get_value("PL1-FL"):
-        if len(plot1_data_x) > plot1_buffer_size:
-            dpg.set_value('PL1-YA-serie', [plot1_data_x[-plot1_buffer_size:], plot1_data_y[-plot1_buffer_size:]])
+    if dpg.get_value("PL1-RB") == "Manuale":
+        dpg.set_axis_limits_auto("PL1-YA")
+
+    if dpg.get_value("PL1-RB") == "Insegui":
+        if len(plot1_data_x) > dpg.get_value("PL1-B"):
+            dpg.set_value('PL1-YA-serie', [plot1_data_x[-dpg.get_value("PL1-B"):], plot1_data_y[-dpg.get_value("PL1-B"):]])
         else:
             dpg.set_value('PL1-YA-serie', [plot1_data_x, plot1_data_y])
 
     else:    
         dpg.set_value('PL1-YA-serie', [plot1_data_x, plot1_data_y])
 
-    if dpg.get_value("PL1-AT") or dpg.get_value("PL1-FL"):
+    if dpg.get_value("PL1-RB") == "Completo" or dpg.get_value("PL1-RB") == "Insegui":
         dpg.fit_axis_data("PL1-XA")
-        dpg.fit_axis_data("PL1-YA")
+        dpg.set_axis_limits("PL1-YA", min(plot1_data_y)-abs(0.1*max(plot1_data_y)), max(plot1_data_y)+abs(0.1*max(plot1_data_y)))
                     
     dpg.render_dearpygui_frame()
 
