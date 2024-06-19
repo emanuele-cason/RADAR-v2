47bca558537760c440a3b5ba31573178491ff51b
Emanuele Cason <emanuele.cason@gmail.com>
2024-06-19 11:08:54 +0200

    Aggiunto codice asincrono per GNSS Sparkfun ublox NEO M8U. DA RICONTROLLARE. Funzionano bene IMU e GPS in sincronia su i2c

--- on-board/on-board.ino
+++ on-board/on-board.ino
@@ -1,8 +1,8 @@
 #include "RF24.h"
 #include "SPI.h"
 #include "nRF24L01.h"
-#include <TinyGPS++.h>
 #include <Wire.h>
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM6DS3TRC.h>
 #include <Adafruit_LIS3MDL.h>
@@ -18,6 +18,9 @@ RF24 radio(4, 5);              // Pin modulo tx/rx CE, CSN
 6 (MOSI) 23
 7 (MISO) 19 */
 
+// Istanza modulo GPS = GNSS
+SFE_UBLOX_GNSS gnss;
+
 // Istanze sensori della IMU
 Adafruit_LSM6DS3TRC lsm6ds;
 Adafruit_LIS3MDL lis3mdl;
@@ -25,14 +28,14 @@ Adafruit_LIS3MDL lis3mdl;
 // Filtro AHRS per dati IMU
 Adafruit_Madgwick filter;
 
-#define GPS_BAUDRATE 9600
-
 const int VOLTAGE_PIN = 35;
 const int CURRENT_PIN = 34;
 
 const byte GS_ADDRESS[5] = {'R', 'x', 'A', 'A', 'A'};
 const int RADIO_CH = 108;
 const int TX_INTERVAL = 100;
+
+const int GNSS_INTERVAL = 500;
 
 // IMU - Frequenza di aggiornamento e valori di calibrazione (da ristabilire quando cambia posizionamento IMU in fusoliera) - ottenuti da MotionCal, seguendo la guida:
 //https://learn.adafruit.com/how-to-fuse-motion-sensor-data-into-ahrs-orientation-euler-quaternions/magnetic-calibration-with-motioncal
@@ -42,35 +45,34 @@ float gyroOffsets[3] = {0.0, 0.0, 0.0}; // giroscopio
 float magHardOffsets[3] = {-3.84, 33.35, -116.58}; // magnetometro
 float magSoftOffsets[9] = {0.98, 0.04, -0.00, 0.04, 1.03, 0.00, -0.00, 0.00, 1.00};
 
-const byte GPS_LOC_PACKET_ID = 1;
-const byte GPS_CLK_PACKET_ID = 2;
+const byte GNSS_LOC_PACKET_ID = 1;
+const byte GNSS_CLK_PACKET_ID = 2;
 const byte POWER_PACKET_ID = 3;
 const byte IMU_PACKET_ID = 4;
 
-TinyGPSPlus gps;
 unsigned long lastTX;
-
 unsigned long lastIMU;
 
-struct gps_loc_packet {
-  uint32_t id = GPS_LOC_PACKET_ID;
+struct gnss_loc_packet {
+  uint32_t id = GNSS_LOC_PACKET_ID;
   uint32_t nSat;
   float lat;
   float lon;
   float alt;
   float speed;
   float cog;
-} gpsLocPacket;
+} gnssLocPacket;
 
-struct gps_clk_packet {
-  uint32_t id = GPS_CLK_PACKET_ID;
+// Momentaneamente non in uso, lasciato per necessità future di avere data/ora a bordo.
+struct gnss_clk_packet {
+  uint32_t id = GNSS_CLK_PACKET_ID;
   uint32_t day;
   uint32_t month;
   uint32_t year;
   uint32_t hour;
   uint32_t minute;
   uint32_t second;
-} gpsClkPacket;
+} gnssClkPacket;
 
 struct power_packet {
   uint32_t id = POWER_PACKET_ID;
@@ -95,14 +97,15 @@ void radioInit() {
       RADIO_CH); // Canale radio, 108 è sopra la maggiorparte delle reti wifi
   radio.setDataRate(RF24_250KBPS); // Livelli possibili: RF24_PA_MIN,
                                    // RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
-  radio.setPALevel(RF24_PA_MIN);
+  radio.setPALevel(RF24_PA_MAX);
   radio.openWritingPipe(GS_ADDRESS);
 }
 
 void imuInit() {
+
   lsm6ds.begin_I2C();
   lis3mdl.begin_I2C();
-  
+
   lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_4_G);
   lsm6ds.setGyroRange(LSM6DS_GYRO_RANGE_500_DPS);
   lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
@@ -136,8 +139,8 @@ void radioTx(void *arg){
 
   while(true){
 
-    radio.write(&gpsLocPacket, sizeof(gpsLocPacket));
-    radio.write(&gpsClkPacket, sizeof(gpsClkPacket));
+    radio.write(&gnssLocPacket, sizeof(gnssLocPacket));
+    //radio.write(&gnssClkPacket, sizeof(gnssClkPacket));
     radio.write(&powerPacket, sizeof(powerPacket));
     radio.write(&imuPacket, sizeof(imuPacket));   
     
@@ -145,39 +148,20 @@ void radioTx(void *arg){
   }
 }
 
-void gpsUpdate(){
-  if (Serial2.available()){
-    if (gps.encode(Serial2.read())) {
+void gnssUpdate(void *arg){
 
-      if (gps.satellites.isValid()) {
-        gpsLocPacket.nSat = gps.satellites.value();
-      }
-
-      if (gps.location.isValid()) {
-        gpsLocPacket.lat = gps.location.lat();
-        gpsLocPacket.lon = gps.location.lng();
-      }
-
-      if (gps.altitude.isValid()){
-        gpsLocPacket.alt = gps.altitude.meters();
-      }
-
-      if (gps.speed.isValid()) {
-        gpsLocPacket.speed = gps.speed.kmph();
-      }
-
-      if (gps.time.isValid()){
-        gpsClkPacket.hour = gps.time.hour();
-        gpsClkPacket.minute = gps.time.minute();
-        gpsClkPacket.second = gps.time.second();
-      }
-
-      if (gps.date.isValid()){
-        gpsClkPacket.day = gps.date.day();
-        gpsClkPacket.month = gps.date.month();
-        gpsClkPacket.year = gps.date.year();
-      }
+  while(true){
+    if (gnss.getPVT()){
+      //Serial.println("Update gnss");
+      gnssLocPacket.nSat = gnss.getSIV();
+      gnssLocPacket.lat = gnss.getLatitude() / 10000000.0;
+      gnssLocPacket.lon = gnss.getLongitude() / 10000000.0;
+      gnssLocPacket.alt = gnss.getAltitudeMSL() / 1000.0 ;
+      gnssLocPacket.speed = gnss.getGroundSpeed() / 1000.0;
+      gnssLocPacket.cog = gnss.getHeading() / 100000.0;
     }
+
+    vTaskDelay(pdMS_TO_TICKS(GNSS_INTERVAL));
   }
 }
 
@@ -231,12 +215,19 @@ void imuUpdate(){
 }
 
 void setup() {
-  Serial2.begin(GPS_BAUDRATE);
   radioInit();
+  
+  Wire.begin();
+  gnss.begin();
+
+  gnss.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  gnss.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
+
   imuInit();
 
   lastTX = 0;
   lastIMU = 0;
+  lastGNSS = 0;
 
   xTaskCreate(
         radioTx,  // Funzione da eseguire
@@ -246,10 +237,20 @@ void setup() {
         1,             // Priorità del task
         NULL           // Puntatore al task handle
     );
+
+    xTaskCreate(
+        gnssUpdate,  // Funzione da eseguire
+        "gnssUpdate",    // Nome del task
+        10000,         // Dimensione dello stack
+        NULL,          // Parametro da passare al task
+        1,             // Priorità del task
+        NULL           // Puntatore al task handle
+    );
+
 }
 
 void loop() {
-  gpsUpdate();
+  
   powerUpdate();
 
   if ((millis() - lastIMU) > (1000/IMU_FREQ)){
